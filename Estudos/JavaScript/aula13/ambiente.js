b77a548fd9b67fa2019846803721a2517651ee32
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2021-12-10 11:35:01 -0300

    Entendendo repetições com Do while e While

---
+++ Estudos/JavaScript/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var contador = 1
+
+while(contador <= 8){
+    console.log(`${contador}: Tudo bem.`)
+    contador++
+}
+
+var contador = 1
+do{
+    console.log(`Passo ${contador}`)
+    contador++
+}while (contador <= 6)
+
+// console.log('1: Tudo bem.')
+// console.log('2: Tudo bem.')
+// console.log('3: udo bem.')
+// console.log('4: Tudo bem.')
+// console.log('5: Tudo bem.')
+// console.log('6: Tudo bem.')
+// console.log('7: Tudo bem.')
+// console.log('8: Tudo bem.')
+    
