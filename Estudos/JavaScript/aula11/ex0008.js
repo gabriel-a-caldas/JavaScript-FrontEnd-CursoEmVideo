d676d309c7faac22ba38292c52e4ebc47403aaac
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2021-12-10 11:11:25 -0300

    Exercícios simples de fixação

---
+++ Estudos/JavaScript/aula11/ex0008.js
@@ -0,0 +1,5 @@
+var vel = 101 
+if (vel > 90){
+    console.log(`A velocidade máxima permitida é 90. Você está multado`)
+}
+console.log(`Dirija sempre com segurança.`)
---
+++ Estudos/JavaScript/aula12/ex013.js
@@ -0,0 +1,12 @@
+
+var horaAtual = new Date()
+
+var hora = horaAtual.getHours()
+
+if (hora < 12){
+    console.log(`Bom dia!`)
+}else if (hora <= 18){
+    console.log(`Boa tarde!`)
+}else{
+    console.log(`Boa noite`)
+}
