28244732483cfce1a0efb52bc3df4db87f692329
Gabriel Caldas <gabrieldeacaldas@gmail.com>
2022-01-06 14:10:17 -0300

    Exercício com contador, trabalhando repetições e lógica

--- Estudos/JavaScript/Aula16-Ex01/script.js
+++ Estudos/JavaScript/Aula16-Ex01/script.js
@@ -1,20 +1,35 @@
 function contar(){
 
-    var inicioContagem = document.getElementById('txtbeg')
-    var fimContagem = document.getElementById('txtend')
-    var passo = document.getElementById('step')
-    var res = document.getElementById('res')
-   
+    let inicioContagem = document.getElementById('txti')
+    let fimContagem = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let resultado = document.getElementById('res')
 
-    if(inicioContagem.value <= 0){
-        res.innerHTML = `Impossível contar!`
+    if(inicioContagem.value.length == 0 || fimContagem.value.length == 0 || passo.value.length == 0){
+        resultado.innerHTML = 'Impossível contar'
     }else{
-        if(inicioContagem.value > fimContagem.value){
-            window.alert('Não estamos trabalhando com contagem reversa')
-        }else if(passo.value == 0){
-            window.alert('Não é possível fazer com passo 0, considerando PASSO 1')
-        }else{
-            
+        resultado.innerHTML = `<p>Contando: <br></p>`
+        let ini = Number(inicioContagem.value)
+        let fim = Number(fimContagem.value)
+        let pas = Number(passo.value)
+        
+        if(pas == 0){
+            window.alert('Passo inválido, considerando passo 1')
+            pas = 1
         }
+
+        if(ini < fim){
+        // contagem crescente
+        for(let aux = ini; aux <= fim; aux += pas){
+                resultado.innerHTML += ` ${aux} \u{1F449}`
+            }
+        }else{
+        // contagem decrescente
+    
+        for(let aux = ini; aux >= fim; aux -= pas){
+             resultado.innerHTML += `${aux} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
     }
 }
